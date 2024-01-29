30076ce91d7e1abce92176ea0adda9fd8b5ac643
Qetzin <qetzin23@gmail.com>
2024-01-29 18:35:03 +0100

    Initial commit

---
+++ Image Steganography/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System.Text;
+using System.Windows.Forms;
+
+namespace Image_Steganography
+{
+    public partial class Form1 : Form
+    {
+        public bool windowEmpty = true;
+        public Bitmap bmp;
+        public Bitmap result;
+
+        private object lockObject = new object();
+        public Form1()
+        {
+            InitializeComponent();
+            if (windowEmpty) 
+            { 
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+        }
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+                n /= 2;
+            }
+
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Choose a Photo";
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                pictureBox1.Image = Image.FromFile(selectedFileName);
+                bmp = (Bitmap)pictureBox1.Image;
+                windowEmpty = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Choose a Photo";
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                pictureBox1.Image = Image.FromFile(selectedFileName);
+                bmp = (Bitmap)pictureBox1.Image;
+                windowEmpty = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!windowEmpty)
+            {
+                Form2 inputForm = new Form2(this, bmp);
+                DialogResult result = inputForm.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string extractedText = String.Empty;
+            if (!windowEmpty)
+            {
+                int colorUnitIndex = 0;
+                int charValue = 0;
+
+                int widthShort = (int)bmp.Width / 5;
+                int heightShort = (int)bmp.Height / 5;
+
+                for (int i = 0; i < heightShort; i++)
+                {
+                    for (int j = 0; j < widthShort; j++)
+                    {
+                        Color pixel = bmp.GetPixel(j, i);
+
+                        for (int n = 0; n < 3; n++)
+                        {
+                            switch (colorUnitIndex % 3)
+                            {
+                                case 0:
+                                    {
+                                        charValue = charValue * 2 + pixel.R % 2;
+                                    }
+                                    break;
+                                case 1:
+                                    {
+                                        charValue = charValue * 2 + pixel.G % 2;
+                                    }
+                                    break;
+                                case 2:
+                                    {
+                                        charValue = charValue * 2 + pixel.B % 2;
+                                    }
+                                    break;
+                            }
+
+                            colorUnitIndex++;
+
+                            if (colorUnitIndex % 8 == 0)
+                            {
+
+                                charValue = reverseBits(charValue);
+                                if (charValue == 0)
+                                {
+
+                                }
+                                char c = (char)charValue;
+                                extractedText += c.ToString();
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show(extractedText, "Decoded Message");
+            }
+        }
+        public void SetResult(Bitmap image)
+        {
+            this.result = image;
+        }
+        public void SetImage()
+        {
+            pictureBox1.Image = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!windowEmpty)
+            {
+                SaveFileDialog ifile = new SaveFileDialog();
+                ifile.FileName = ".png";
+
+                if (ifile.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(ifile.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
---
+++ Image Steganography/Form2.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Image_Steganography
+{
+    public partial class Form2 : Form
+    {
+        Bitmap bmp;
+        Form1 reference;
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
+        public Form2(Form1 formO,Bitmap btm)
+        {
+            bmp = btm;
+            reference = formO;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            State state = State.Hiding;
+            int charIndex = 0;
+            int charValue = 0;
+            long pixelElementIndex = 0;
+            int zeros = 0;
+            int R = 0, G = 0, B = 0;
+
+            int widthShort = (int)bmp.Width / 5;
+            int heightShort = (int)bmp.Height / 5;
+
+            for (int i = 0; i < heightShort; i++)
+            {
+                for (int j = 0; j < widthShort; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                reference.SetResult(bmp);
+                                reference.SetImage();
+                                this.Close();
+                            }
+                            if (charIndex >= text.Length)
+                            {
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        R += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+
+                        pixelElementIndex++;
+
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            reference.SetResult(bmp);
+            reference.SetImage();
+            this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
